97182d56b1baaba57067ede28a1a193d99939c60
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-11-13 02:11:38 +0600

    sucsess fully create table

--- src/Body/AllCatagory/TeachersAndStaff/Computer.jsx
+++ src/Body/AllCatagory/TeachersAndStaff/Computer.jsx
@@ -25,12 +25,47 @@ export default function Computer() {
                   <td className={`${style}`}>
                     <table className="border-collapse border border-slate-400 text-[13px]">
                         <tr>
+                            <th className={`${style} w-36 p-[5px]`}> <img  src={maminulIslam} alt="Computer" /></th>
+                            <td className='p-3'>
+                                <table className="border-collapse border border-slate-400 text-[13px] text-start p-3">
+                                        <tr>
+                                            <td className={`${style} font-semibold w-20`} colSpan={"1"}> নাম</td>
+                                            <td className={`${style} font-semibold w-20`} colSpan={"3"}> মোঃ মমিনুল ইসলাম</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={`${style}`}>পদবি</td>
+                                            <td className={`${style}`} colSpan={"3"}>চিফ ইন্সট্রাক্টর ও বিভাগীয় প্রধান (টেক/কম্পিউটার)</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={`${style}`}>মোবাইল</td>
+                                            <td className={`${style}`}>01716514614</td>
+                                            <td className={`${style}`}>ফোন ( অফিস )</td>
+                                            <td className={`${style}`}>05555555555</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={`${style}`}>ই-মেইল</td>
+                                            <td className={`${style}`}>maminulins@gmail.com</td>
+                                            <td className={`${style}`}>পি. আই. এম. এস.</td>
+                                            <td className={`${style}`}>-</td>
+                                        </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            {/* teacher 02 */}
+            <tr>
+                  <td className={`${style}`}>1</td>
+                  <td className={`${style}`}>
+                    <table className="border-collapse border border-slate-400 text-[13px]">
+                        <tr>
                             <th className={`${style} w-36 p-[5px]`}> <img  src={alamgirSir} alt="Computer" /></th>
                             <td className='p-3'>
                                 <table className="border-collapse border border-slate-400 text-[13px] p-3">
                                         <tr>
-                                            <th className={`${style} p-[5px] w-20`} colSpan={"1"}> নাম</th>
-                                            <th className={`${style} p-[5px] `} colSpan={"3"}> মোঃ আলমগীর</th>
+                                            <td className={`${style} font-semibold w-20`} colSpan={"1"}> নাম</td>
+                                            <td className={`${style} font-semibold w-20`} colSpan={"3"}> মোঃ আলমগীর</td>
                                         </tr>
                                         <tr>
                                             <td className={`${style}`}>পদবি</td>
@@ -54,9 +89,51 @@ export default function Computer() {
                     </table>
                 </td>
             </tr>
-            {/* teacher 02 */}
+            {/* teacher 03 */}
+            <tr>
+                  <td className={`${style}`}>1</td>
+                  <td className={`${style}`}>
+                    <table className="border-collapse border border-slate-400 text-[13px]">
+                        <tr>
+                            <th className={`${style} w-36 p-[5px]`}> <img  src={shafic} alt="Computer" /></th>
+                            <td className='p-3'>
+                                <table className="border-collapse border border-slate-400 text-[13px] p-3">
+                                        <tr>
+                                            <td className={`${style} font-semibold w-20`} colSpan={"1"}> নাম</td>
+                                            <td className={`${style} font-semibold w-20`} colSpan={"3"}> মোঃ শফিকুল ইসলাম</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={`${style}`}>পদবি</td>
+                                            <td className={`${style}`} colSpan={"3"}>ইন্সট্রাক্টর / কম্পিউটার (প্রসিড টু রেভিনিউ )</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={`${style}`}>মোবাইল</td>
+                                            <td className={`${style}`}>01736507993</td>
+                                            <td className={`${style}`}>ফোন ( অফিস )</td>
+                                            <td className={`${style}`}>05555555555</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={`${style}`}>ই-মেইল</td>
+                                            <td className={`${style}`}>shafiqwish@gmail.com</td>
+                                            <td className={`${style}`}>পি. আই. এম. এস.</td>
+                                            <td className={`${style}`}>-</td>
+                                        </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            {/* teacher 4 */}
         </table>
           
       </div>
+    )
+}
+
+
+function TableData(){
+    return(
+        
     )
 }
