cba3736654566fdc2530c5a3d1b1b2a2a784dfe1
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-11-13 01:55:30 +0600

    work on complicate teacher table

--- src/Body/AllCatagory/TeachersAndStaff/Computer.jsx
+++ src/Body/AllCatagory/TeachersAndStaff/Computer.jsx
@@ -1,24 +1,62 @@
-
+import alamgirSir from '../../../../img/TeacherAndSTaff/Computer/alamgirSir.jpg'
+import anik from '../../../../img/TeacherAndSTaff/Computer/anik.jpg'
+import arzina from '../../../../img/TeacherAndSTaff/Computer/arzina.jpg'
+import bikash from '../../../../img/TeacherAndSTaff/Computer/bikash.jpg'
+import faisal from '../../../../img/TeacherAndSTaff/Computer/bikash.jpg'
+import maminulIslam from '../../../../img/TeacherAndSTaff/Computer/maminulIslam.jpg'
+import romjan from '../../../../img/TeacherAndSTaff/Computer/romjan.jpg'
+import shafic from '../../../../img/TeacherAndSTaff/Computer/shafiq.png'
 
 import { Link } from "react-router-dom"
 
 export default function Computer() {
-    const style='border border-slate-300 p-5'
+    const style='border border-slate-300 p-4'
 
     return (
       <div>
-          <table className="border-collapse border border-slate-400 text-[13px]">
+        <table className="border-collapse border border-slate-400 text-[13px]">
               <tr>
                   <th className={`${style} w-2`}>#</th>
                   <th className={`${style}`}>বিস্তারিত</th>
               </tr>
-  
+                {/* teacher 01 */}
               <tr>
                   <td className={`${style}`}>1</td>
-                  <td className={`${style}`}><Link>পর্ব সমাপনী পরীক্ষা ২২ এর ২৩/০৭/২৩ ইং তারিখ বিকাল ২.০০ টার নিয়মিত এবং রেফার্ড আসন বিন্যাস।</Link></td>
-              </tr>
-              
-          </table>
+                  <td className={`${style}`}>
+                    <table className="border-collapse border border-slate-400 text-[13px]">
+                        <tr>
+                            <th className={`${style} w-36 p-[5px]`}> <img  src={alamgirSir} alt="Computer" /></th>
+                            <td className='p-3'>
+                                <table className="border-collapse border border-slate-400 text-[13px] p-3">
+                                        <tr>
+                                            <th className={`${style} p-[5px] w-20`} colSpan={"1"}> নাম</th>
+                                            <th className={`${style} p-[5px] `} colSpan={"3"}> মোঃ আলমগীর</th>
+                                        </tr>
+                                        <tr>
+                                            <td className={`${style}`}>পদবি</td>
+                                            <td className={`${style}`} colSpan={"3"}>ইন্সট্রাক্টর / কম্পিউটার (প্রসিড টু রেভিনিউ )</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={`${style}`}>মোবাইল</td>
+                                            <td className={`${style}`}>01724334828</td>
+                                            <td className={`${style}`}>ফোন ( অফিস )</td>
+                                            <td className={`${style}`}>05555555555</td>
+                                        </tr>
+                                        <tr>
+                                            <td className={`${style}`}>ই-মেইল</td>
+                                            <td className={`${style}`}>alamgircse13@gmail.com</td>
+                                            <td className={`${style}`}>পি. আই. এম. এস.</td>
+                                            <td className={`${style}`}>-</td>
+                                        </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            {/* teacher 02 */}
+        </table>
+          
       </div>
     )
 }
