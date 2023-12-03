28fb1623f47c685de19dba78d153171526a3c606
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-12-03 23:01:34 +0600

    change the student sucsess history

--- src/Body/AllCatagory/StudentSuccessStories.jsx
+++ src/Body/AllCatagory/StudentSuccessStories.jsx
@@ -1,57 +1,29 @@
-import { Link } from "react-router-dom"
+import person from "../../../public/img/icons8-person-100.png";
 
 export default function StudentSuccessStories() {
-    const style='border border-slate-300 p-3 max-w-full'
+  const style = "border border-slate-300 text-xs md:text-base p-3 w-full ";
 
-    return (
-      <div className="overflow-x-auto mb-5 max-w-full">
-          <table className="border-collapse border border-slate-400 text-justify text-[12px] ${style} sm:w-full">
-              <tr>
-                  <th className={`${style}`}>Identity</th>
-                  <th className={`${style}`}>Office</th>
-                  <th className={`${style}`}>Contact</th>
-              </tr>
-  
-              <tr>
-                  <td className={`${style}`}>Full Name : Abdur Rahim
-                    Roll Number : 707242
-                    Technology : 66-COMPUTER
-                    Passing Year : 2017</td>
-                  <td className={`${style}`}><Link>Designation : <span className="bg-[#00C851] px-2 text-white rounded-md">Branch Manager</span><br />
-                    Name Of Office : Paltan, Dhaka-1000
-                    Office Address : Eastern Arzoo Tower, Level 5; Suite #05-261 Bijoy Nagar Road, Dhaka-1000</Link></td>
-                  <td className={`${style} `}><Link>Email : arahim.ict.bd@gmail.com
-                    Facebook Link : https://www.facebook.com/Abdur.Rahim.tech/</Link></td>
-              </tr>
-              
-              <tr>
-                  <td className={`${style}`}>Full Name : Abdur Rahim
-                    Roll Number : 707242
-                    Technology : 66-COMPUTER
-                    Passing Year : 2017</td>
-                  <td className={`${style}`}><Link>Designation : <span className="bg-[#00C851] px-2 text-white ">Branch Manager</span><br />
-                    Name Of Office : Paltan, Dhaka-1000
-                    Office Address : Eastern Arzoo Tower, Level 5; Suite #05-261 Bijoy Nagar Road, Dhaka-1000</Link></td>
-                  <td className={`${style} `}><Link>Email : arahim.ict.bd@gmail.com
-                    Facebook Link : https://www.facebook.com/Abdur.Rahim.tech/</Link></td>
-              </tr>
-
-
-              <tr>
-                  <td className={`${style}`}>Full Name : Abdur Rahim
-                    Roll Number : 707242
-                    Technology : 66-COMPUTER
-                    Passing Year : 2017</td>
-                  <td className={`${style}`}><Link>Designation : <span className="bg-[#00C851] px-2 text-white ">Branch Manager</span><br />
-                    Name Of Office : Paltan, Dhaka-1000
-                    Office Address : Eastern Arzoo Tower, Level 5; Suite #05-261 Bijoy Nagar Road, Dhaka-1000</Link></td>
-                  <td className={`${style} `}><Link>Email : arahim.ict.bd@gmail.com
-                    Facebook Link : https://www.facebook.com/Abdur.Rahim.tech/</Link></td>
-              </tr>
-
-              
-              
-          </table>
+  return (
+    <div className="border-[1px] border-slate-200 w-full p-3 flex gap-10 items-center rounded-lg shadow-xl shadow-slate-400">
+      <aside className=" flex items-center justify-center w-44">
+        <img className=" w-full border-[1px] border-slate-400 rounded-full" src={person} alt={person} />
+      </aside>
+      <div className=" flex flex-wrap justify-evenly w-full capitalize font-semibold text-sm">
+        <div className=" flex flex-col gap-1">
+          <h2 className="bg-gradient-to-r from-[#1B8119] to-[#46A20D] py-1 text-white border-l-[4px] border-red-600 pl-3">identity</h2>
+          <p>name : mujahid</p>
+          <p>department : computer</p>
+          <p>session : 2019/20</p>
+          <p>passing year : 2024</p>
+          <p>CGPA : 3.75</p>
+        </div>
+        <div className=" flex flex-col gap-1">
+          <h2 className="bg-gradient-to-r from-[#1B8119] to-[#46A20D] py-1 text-white border-l-[4px] border-red-600 pl-3">designation</h2>
+          <p>position : junior frontend developer</p>
+          <p>Mobail : 01737705511</p>
+          <p>email : mujahidislam400301@gmail.com</p>
+        </div>
       </div>
-    )
+    </div>
+  );
 }
